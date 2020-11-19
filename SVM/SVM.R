cf15536e0678677ff5c337786bd41a0b6248e867
fholstege <31654652+fholstege@users.noreply.github.com>
2020-11-19 19:41:52 +0100

    quadratic function

--- SVM/SVM.R
+++ SVM/SVM.R
@@ -6,7 +6,8 @@ setwd("C:/Users/flori/OneDrive/Documents/GitHub/Supervised ML Code/SVM")
 if (!require("pacman")) install.packages("pacman")
 pacman::p_load(fastDummies,
                SVMMaj,
-               dplyr)
+               dplyr,
+               caret)
 
 
 # load bank data
@@ -50,31 +51,55 @@ numeric_vars <- as.matrix(data.frame(mX$age,
 # combine numeric and dummy variables
 mX_var <- as.matrix(cbind(1, scale(numeric_vars),dummy_vars))
 
-
-create_hinge_param <- function(mY, z, type_hinge = "absolute", epsilon = 1e-08){
-  
-  m <- abs(1 - z)
-  m <- m * (m > epsilon) + epsilon * (m <= epsilon)
-
-  a <- .25 * m^-1
-  b <- mY * (a + 0.25)
-  c <- a + 0.5 + 0.25 * m
-  
-  result = list(a = a,
-                b = b,
-                c = c)
-}
-
-calc_loss <- function(z, lambda, mWtW, type_loss = "absolute"){
-  
-  #penalty = lambda * mWtW
-  
-  vloss <- (1 > z) * (1 - z)# + penalty[[1]]
-  
-  return(sum(vloss))
-}
-
 getHinge
+
+create_hinge_param <- function(mY, z, hinge = "absolute", epsilon = 1e-08, delta =1){
+  
+  if(hinge == "absolute"){
+  
+    m <- abs(1 - z)
+    m <- m * (m > epsilon) + epsilon * (m <= epsilon)
+  
+    a <- .25 * m^-1
+    b <- mY * (a + 0.25)
+    
+    return(result = list(a = a,
+                          b = b))
+
+  }else if (hinge == "quadratic"){
+    
+    m <- z* (z > 1) + (1 >= z)
+    b <- m * mY
+    
+    return(list(b = b))
+    
+  }else if(hinge == "huber"){
+    
+    m <- (z <- delta) * (z + delta)
+  }
+  
+}
+
+
+calc_loss <- function(z, mW, lambda, hinge = "absolute"){
+  
+  mWtW = t(mW) %*% mW
+  penalty = lambda * mWtW
+  
+  if(hinge == "absolute"){
+    
+    vloss <- (1 > z) * (1 - z)
+    
+  }else if(hinge == "quadratic"){
+    
+    
+    vloss <- (1 > z) * (1 - z)^2
+  }
+
+  return(sum(vloss) + penalty)
+}
+
+
 
 
 svm_mm <- function(mY, mX, hinge = "absolute", lambda = 10, epsilon = 1e-08){
@@ -96,6 +121,10 @@ svm_mm <- function(mY, mX, hinge = "absolute", lambda = 10, epsilon = 1e-08){
   k = 1
   stepscore = 0 
   
+  if(hinge == "quadratic"){
+    Z = inv(t(mX) %*% mX + lambda * P) %*% t(mX)
+  }
+  
   
   while(k ==1 || stepscore > epsilon){
     
@@ -104,32 +133,40 @@ svm_mm <- function(mY, mX, hinge = "absolute", lambda = 10, epsilon = 1e-08){
     fCurrent_z = mY * mCurrent_q
     
     # get parameters given the y and z
-    lhinge_param = create_hinge_param(mY, fCurrent_z, epsilon)
+    lhinge_param = create_hinge_param(mY, fCurrent_z, hinge, epsilon)
     
-    # define A and b
-    A = diag(as.vector(lhinge_param$a))
     b = lhinge_param$b
     
-    # get updated v
-    vV_update = solve(t(mX) %*% A %*% mX + lambda * P, t(mX) %*% b)
+    if(hinge == "quadratic"){
+      
+      vV_update = Z %*% b
+      
+    }else{
+      
+      # define A and b
+      A = diag(as.vector(lhinge_param$a))
+      
+      # get updated v
+      vV_update = solve(t(mX) %*% A %*% mX + lambda * P, t(mX) %*% b)
+      
+    }
     
-    mW = tail(vV_update,-1)
-    mWtW = t(mW) %*% mW
+    mW_previous = tail(vV_previous,-1)
+    mW_update = tail(vV_update, -1)
+
 
     # get new prediction (q) and z
     mNew_q = mX %*% vV_update
     fNew_z = mY * mNew_q
     
     # calculate new, and previous loss
-    fCurrent_loss = calc_loss(fCurrent_z, lambda, mWtW)
-    fNew_loss = calc_loss(fNew_z, lambda, mWtW)
+    fCurrent_loss = calc_loss(fCurrent_z, mW_previous, lambda, hinge)
+    fNew_loss = calc_loss(fNew_z,mW_update, lambda, hinge)
     
     # calculate improvement
     stepscore <- (fCurrent_loss - fNew_loss)/fCurrent_loss
     
-    print("improvement")
-    print(stepscore)
-    
+
     # check: if all predicted correctly, turns to NaN since divided by zero
     if (is.na(stepscore)){
       stepscore = 0
@@ -138,32 +175,26 @@ svm_mm <- function(mY, mX, hinge = "absolute", lambda = 10, epsilon = 1e-08){
     # move to next iteration
     k = k + 1
     vV_previous = vV_update
-    
-    
+  
     
     
   }
   
   mYhat = sign(mNew_q)
   
-  #vPredict_outcome = ifelse(
-       #  mYhat == mY == 1, "true positive",
-       #  mYhat == mY == -1, "true negative",
-       #  myhat == 1, my == -1, "false positive", 
-       #  myhat == -1, my == 1, "false negative")
-  
-  
-  #dfPredict_overview = table(vPredict_outcome)
-  
-  
-  
+  resultOverview <- confusionMatrix(as.factor(mY), as.factor(mYhat))
+  mConfusionTable <- resultOverview$table
+  fAccuracy <- resultOverview$overall[1]
+
   
   lResults = list(v = vV_update,
                   mY = mY,
                   mX = mX, 
                   loss = fCurrent_loss,
                   q = mNew_q,
-                  yhat = mYhat)
+                  yhat = mYhat,
+                  Accuracy = fAccuracy,
+                  ConfusionTable = mConfusionTable)
   
   return(lResults)
 
@@ -172,46 +203,34 @@ svm_mm <- function(mY, mX, hinge = "absolute", lambda = 10, epsilon = 1e-08){
 }
 
 
+
+
 # ensure that we can reproduce the code
 set.seed(1)
 
+# pick a random sample of 1000
 sample_id <- sample(4000, 1000)
-
-
 sample_x = mX_var[sample_id,]
 sample_y = mY_num[sample_id]
 
-result = svm_mm(sample_y, sample_x)
+
+# compare our algorithm with SVM Maj - we get the same predictions (see confusion matrix), and exactly the same loss
+# but still different beta's? most likely something to do with the transformation of the data...
+result = svm_mm(sample_y, sample_x, lambda = 100, hinge = "quadratic")
 result$v
 result$loss
-
-mYhat = result$yhat
-sample_y <- as.matrix(sample_y)
-mYhat
-vPredict_outcome = ifelse(
-  (mYhat == sample_y & sample_y == 1), "true positive",
-  ifelse((mYhat == sample_y & sample_y == -1), "true negative",
-  ifelse((mYhat == 1 & sample_y == -1), "false positive", 
-  ifelse((mYhat == -1 & sample_y == 1), "false negative", NA))))
+result$ConfusionTable
+result$Accuracy
 
 
-
-
-dfPredict_overview = table(vPredict_outcome)
-dfPredict_overview
-
-
-result_svmmaj <- svmmaj(sample_x,sample_y, lambda = 10, scale = "none",hinge = "absolute")
+result_svmmaj <- svmmaj(sample_x,sample_y, lambda = 100, scale = "none",hinge = "quadratic")
 result_svmmaj$beta 
 result_svmmaj$loss
 result_svmmaj$q
 
 
 
-# check score
 
-# get current prediction (q) and z
-mCurrent_q = sample_x %*% result_svmmaj$beta 
-fCurrent_z = sample_y* mCurrent_q
 
-calc_loss(fCurrent_z)
+create_hinge_param()
+
